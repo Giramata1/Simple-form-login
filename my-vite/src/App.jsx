7fe10c168f555e06d24c0ffe6434dade0e532103
Giramata1 <amugenigiramata@gmail.com>
2025-04-30 16:36:57 +0200

    Create App.jsx

---
+++ my-vite/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SimpleForm from './components/Validation/SimpleForm';
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+    <SimpleForm/>
+    </div>
+  );
+}
+
+export default App
